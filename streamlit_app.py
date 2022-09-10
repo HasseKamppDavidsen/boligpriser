af906f3c4282843be9bed7440dc8aca7c845f374
HasseKamppDavidsen <hassedavidsen@gmail.com>
2022-09-11 00:37:58 +0200

    changes

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,10 +7,12 @@ import json
 import requests
 import pandas as pd
 import plotly.express as px
+import plotly.graph_objects as go
 import geopy.distance
 from streamlit_folium import folium_static
 import folium
 import openpyxl
+import matplotlib as mpl
 
 #st.set_page_config(layout="wide")
 
@@ -44,7 +46,7 @@ def fnc_getAdressCoordinates(adressStr: str, postNr: int):
 
 # Get sold properties
 @st.cache(allow_output_mutation=True, show_spinner=False) # Mutations??
-def fnc_getSoldData(postNr: list, soldYear: int):
+def fnc_getSoldData(postNr: list, soldYear: int, dfPropType):
 
     with st.spinner('Henter salgsdata fra Boliga...'):
         
@@ -71,16 +73,18 @@ def fnc_getSoldData(postNr: list, soldYear: int):
 
                 df = pd.concat([df, dftmp])
 
-            dfSold = pd.concat([dfSold, df]) 
-    
-    #st.success('Done!')
+            dfSold = pd.concat([dfSold, df])
 
+            for index, row in dfPropType.iterrows():
+                dfSold.loc[dfSold['propertyType'] == row['ID'], "propertyName" ] = row['Type']
+    
     return dfSold  
 
 #@st.cache
 def fnc_findAdressInRadius(dfAdress, dfSold, propType):
     
     dfSold['Distance_m'] = "" 
+    dfPropFilter = pd.DataFrame()
 
     if len(dfAdress)>0:
         coords_adr = (dfAdress['adgangsadresse.y'], dfAdress['adgangsadresse.x'])
@@ -91,10 +95,27 @@ def fnc_findAdressInRadius(dfAdress, dfSold, propType):
     for index, row in dfSold.iterrows():
         coords_sold = (row['latitude'], row['longitude'])
         distm = geopy.distance.geodesic(coords_adr, coords_sold).m
-        #print(index, coords_adr, coords_sold, distm)
         dfSold.loc[index, 'Distance_m'] = distm
-        
+
+    if len(propType)>0:    
+        for propID in propType:
+            dfPropFilter_tmp = dfSold.query("propertyType == @propID")
+            dfPropFilter = pd.concat([dfPropFilter, dfPropFilter_tmp])
+        dfSold = dfPropFilter
+
+    dfSold['roll_sqmPrice'] = dfSold.sqmPrice.rolling(50).mean()
+    dfSold['roll_Price'] = dfSold.price.rolling(50).mean()
+
+    dfSold.sort_values(by=['soldDate'], inplace=True)
+
     return dfSold
+
+def fnc_findColorValue(dfSoldDistFilt):
+    sqmPriceMax = dfSoldDistFilt['sqmPrice'].max()
+    sqmPriceMin = dfSoldDistFilt['sqmPrice'].min()
+    dfSoldDistFilt['ColorScale'] = (dfSoldDistFilt['sqmPrice']-sqmPriceMin)/(sqmPriceMax-sqmPriceMin)
+
+    return dfSoldDistFilt
 
 # Generate city name in multiselect
 def fnc_getName(pstNr, dfpst):
@@ -104,6 +125,18 @@ def fnc_getName(pstNr, dfpst):
 def fnc_getPropertyName(propID, dfProp):
     propStr = dfProp.query("ID == @propID").iloc[0,1] 
     return str(propStr)
+
+def fnc_findColor(priceval):
+    myColor = [(2*priceval),(2*(1 - priceval)),0]
+    newColor = []
+
+    for c in myColor:
+        if c > 1:
+            newColor.append(1)
+        else:
+            newColor.append(c)
+
+    return mpl.colors.to_hex(newColor)
 
 dfPropType = pd.DataFrame({'ID':[1,2,3,4,5], 'Type':['Villa', 'Rækkehus', 'Ejerlejlighed', 'Fritidshus', 'Landejendom']})
 
@@ -134,6 +167,8 @@ if 'ssHentData' not in st.session_state:
 # Sidebare
 with st.sidebar:
     
+    st.title("Solgte boliger i DK")
+
     dfpst = fnc_getPostalCode()
     
     with st.form("formHentData"):
@@ -141,19 +176,26 @@ with st.sidebar:
         soldYear = st.number_input("Tidligste salgsår", value=2021, min_value=1990, format="%i")
         adresseStr = st.text_input("Skriv adresse der ligger i det først valgte postnummer", value="Hjertebjergvej 12")
         distFilt_m = st.number_input("Søge afstand fra adresse", value=500, step=250, min_value=100, max_value=10000, format="%i")
-        propType = st.multiselect("Vælg boligtype", dfPropType["ID"], format_func=lambda propNr: fnc_getPropertyName(propNr, dfPropType))
+        propType = st.multiselect("Vælg boligtype", dfPropType["ID"],help="Hej", format_func=lambda propNr: fnc_getPropertyName(propNr, dfPropType))
         blShowPostCd = st.checkbox("Vis postnummer på kortet")
 
         submitHentData = st.form_submit_button("Opdater visning")
 
         if submitHentData:
             if len(postNr)>0:
-                dfSold = fnc_getSoldData(postNr, soldYear)
+                dfSold = fnc_getSoldData(postNr, soldYear, dfPropType)
                 dfadr = fnc_getAdressCoordinates(adresseStr, postNr[0])
                 if len(dfadr.index)>0:
                     dfSoldDist = fnc_findAdressInRadius(dfadr, dfSold, propType)
-                    dfSoldDistFilt = dfSoldDist.query("Distance_m < @distFilt_m")
-                    st.session_state['ssHentData'] = True
+                    if len(dfSoldDist.index)>0:
+                        dfSoldDistFilt = dfSoldDist.query("Distance_m < @distFilt_m")
+                        dfSoldDistFilt = fnc_findColorValue(dfSoldDistFilt)
+                        if len(dfSoldDistFilt.index)>0:
+                            st.session_state['ssHentData'] = True
+                        else:
+                            st.warning('Ingen solgte boliger blev fundet', icon="⚠️")
+                    else:
+                        st.warning('Ingen solgte boliger blev fundet', icon="⚠️")
                 else:                
                     st.warning('Adressen blev ikke fundet inden for det først valgte postnummer', icon="⚠️")
                 
@@ -162,12 +204,6 @@ with st.sidebar:
 
 #######################################################################################
 # Main page
-
-
-# st.dataframe(dfSold)
-# st.dataframe(dfadr)
-# st.dataframe(dfSoldDist)
-# st.dataframe(dfSoldDist)
 
 if len(dfSoldDist.index)>0:
     coords_adr = (dfadr['adgangsadresse.y'], dfadr['adgangsadresse.x'])
@@ -210,7 +246,7 @@ if len(dfSoldDist.index)>0:
             popup="Adresse: " + str(row['address']) + " Type: " + str(row['propertyType']),
             color='#000000',
             fill=True,
-            fill_color='#ffffff',
+            fill_color=fnc_findColor(row['ColorScale']),
             fill_opacity=0.7,
             parse_html=False).add_to(featGrp_Solgte) 
 
@@ -218,22 +254,92 @@ if len(dfSoldDist.index)>0:
 
         folium.LayerControl().add_to(map)
 
-        with st.expander("Oversigtskort", expanded=True):
+        with st.expander("Oversigtskort", expanded=False):
             folium_static(map, width=670, height=500)
         
-        with st.expander("Plot over priser"):
-            fig = px.scatter(dfSoldDistFilt, x='soldDate', y='price')#, color='GEOL_LEG_C', symbol='GEOL_LEG_C')
+        with st.expander("Plot over priser", expanded=True):
+
+            #yaxisParameter = st.selectbox("Vælg parameter til graf", ['Kvadratmeterpriser','Salgspriser'])
+
+            yaxisParameter = 'Kvadratmeterpriser'
+
+            if yaxisParameter == 'Salgspriser':
+                yValue = 'price'
+                yValueRoll = 'roll_Price'
+                yAxisTitle = 'Salgspris (DKK)'
+            else:
+                yValue = 'sqmPrice'
+                yValueRoll = 'roll_sqmPrice'
+                yAxisTitle = 'Kvadratmeterpris (DKK/m2)'
+
+            fig = px.scatter(
+                dfSoldDistFilt, 
+                x='soldDate', 
+                y=yValue, 
+                color="propertyName", 
+                labels={"propertyName": "Boligtype"}
+                )
+
+            fig.update_layout(
+                # title={
+                #     'text': 'Indsæt titel her',
+                #     'y':1.0,
+                #     'x':0.0,
+                #     #'xanchor': 'center',
+                #     #'yanchor': 'top'
+                #     },
+                yaxis_title=yAxisTitle,
+                xaxis_title="",
+                #xaxis={'side':'top'},
+                width = 670,
+                height = 500,
+                paper_bgcolor='rgba(0,0,0,0)',
+                plot_bgcolor='rgba(0,0,0,0)'
+            )
+
+            fig.add_trace(
+                go.Scatter(
+                x=dfSoldDistFilt["soldDate"], 
+                y=dfSoldDistFilt[yValueRoll],
+                line=go.scatter.Line(color="gray"),
+                showlegend=False)
+                )
+
+            fig.update_xaxes(
+                showline=True, 
+                linewidth=1, 
+                mirror=True, 
+                gridcolor='Black', 
+                linecolor='black'
+                )
+            fig.update_yaxes(
+                showline=True, 
+                linewidth=1, 
+                mirror=True, 
+                gridcolor='Black',
+                linecolor='black'
+                )
+
             st.plotly_chart(fig)
     
         with st.expander("Rådata"):
             st.dataframe(dfSoldDistFilt)
     
 
-# else:
-#     if st.session_state['ssHentData'] == False:
-#         st.header("Hent først data for én eller flere postnumre")
-#     elif st.session_state['ssHentData'] == True:
-#         st.header("Angiv adresse og opdater visning")
+else:
+    if st.session_state['ssHentData'] == False:
+        st.header("Hent først data for én eller flere postnumre")
+    elif st.session_state['ssHentData'] == True:
+        st.header("Angiv adresse og opdater visning")
+    
+    with st.expander("Oversigtskort", expanded=False):
+        st.write("Intet at vise")
+        
+    with st.expander("Plot over priser"):
+        st.write("Intet at vise")
+
+    with st.expander("Rådata"):
+        st.write("Intet at vise")
 
 
 
@@ -308,3 +414,30 @@ if len(dfSoldDist.index)>0:
 # folium_map
 
 #dfPropType = pd.DataFrame({'ID':[1,2,3,4,5], 'Type':['Villa', 'Rækkehus', 'Ejerlejlighed', 'Fritidshus', 'Landejendom']})
+
+
+# import matplotlib
+# import matplotlib.pyplot as plt
+# import numpy as np
+
+# x = 0.1
+
+# myColor = [(2*x),(2*(1 - x)),0]
+# newColor = []
+
+# for c in myColor:
+#     if c > 1:
+#         newColor.append(1)
+#     else:
+#         cc = c
+#         newColor.append(cc)
+
+# res = matplotlib.colors.to_hex(newColor)
+# print(res)
+# print(newColor)
+
+# xpoints = np.array([1, 8])
+# ypoints = np.array([3, 10])
+
+# plt.plot(xpoints, ypoints, res)
+# plt.show()
